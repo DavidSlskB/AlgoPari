c5b9edd62f95fcf867f220f3e695d8a40e98b90b
DavidSlskB <slaskibertdavid@gmail.com>
2025-08-11 00:14:10 +0200

    add logs

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from core.config import generate_configurations
 from core.simulator import run_scraping_pipeline
 from core.result_updater import update_pending_results
 from core.stats_generator import calculate_statistics
+import sys
+from datetime import datetime
+
+sys.stdout = open(f"logs/log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt", "w", encoding="utf-8")
 
 def main():
     print("\n=== Étape 1 : Scraping FDJ ===")
